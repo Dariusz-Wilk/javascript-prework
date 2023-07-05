dba261764b31976afd11945776e2b3ff295e83c5
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-07-05 11:55:35 +0100

    create if...else block for computer move

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-let computerMove = `kamień`;
-let playerMove = `papier`;
+let computerMove = 'nieznany ruch';
+let playerMove = 'nieznany ruch';
 
 printMessage(
 	'Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!'
 );
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+if (randomNumber == 1) {
+	computerMove = 'kamień';
+} else if (randomNumber == 2) {
+	computerMove = `papier`;
+} else {
+	computerMove = `nożyce`;
+}
+
+printMessage('Mój ruch to: ' + computerMove);
